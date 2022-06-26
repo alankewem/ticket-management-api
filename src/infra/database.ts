9960ca42020ef5feb4a12f6273196a8eb5b69b0d
Alan Kewem <kewemcg@gmail.com>
2022-06-26 13:21:54 -0400

    infra: MongoDB class for database connection

---
+++ src/infra/database.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_PORT, MONGO_DB } = process.env;
+
+export class MongoDB {
+  public async connection() {
+    return mongoose
+      .connect(this.URL(), this.options)
+      .then(() => {
+        console.log("MongoDB is connected");
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
+  private options = {
+    useNewUrlParser: true,
+    connectTimeoutMS: 10000,
+  };
+
+  private URL() {
+    return `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+  }
+}
