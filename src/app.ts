442fef9a7a9458d5b665c3b5685a67761b0b5982
Alan Kewem <kewemcg@gmail.com>
2022-06-26 13:21:00 -0400

    feat: express middlewares and database connection

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { MongoDB } from "./infra/database";
+
+class App {
+  public server: express.Application;
+
+  constructor() {
+    this.server = express();
+    this.middleware();
+    this.router();
+    this.database();
+  }
+  private async database() {
+    console.log("connecting...");
+    const mongodb = new MongoDB();
+    await mongodb.connection();
+  }
+
+  private middleware() {
+    this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: true }));
+    this.server.use(cors());
+  }
+
+  private router() {
+    this.server.use((req, res, next) => {
+      res.status(404);
+      res.send({
+        error: "not found",
+      });
+    });
+  }
+}
+
+export default new App().server;
