254d5027a7746279ad4712e7bf84097b382ed7b4
Alan Kewem <kewemcg@gmail.com>
2022-06-28 22:30:10 -0400

    feat: event repository interface

---
+++ src/repository/IEventRepository.ts
@@ -0,0 +1,7 @@
+import { Event } from "../entities/Event";
+
+export interface IEventRepository {
+  findById(id: string): Promise<Event>;
+  findByTitle(text: string): Promise<Event[]>
+  save(event: Event): Promise<void>;
+}
