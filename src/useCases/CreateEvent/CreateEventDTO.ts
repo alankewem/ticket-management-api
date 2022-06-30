137358f39faa3a6b7edad1df6b82444da41ba3f0
kewem <kewemcg@gmail.com>
2022-06-30 10:27:45 -0400

    fix: change DTO address to string instead object

--- src/useCases/CreateEvent/CreateEventDTO.ts
+++ src/useCases/CreateEvent/CreateEventDTO.ts
@@ -1,10 +1,8 @@
-import { IAddress } from "../../entities/Event"
-
 export interface CreateEventDTO {
     title: string
     description: string
     eventDate: string
     price: number
     ticketsAvailable: number
-    address: IAddress
+    address: string
 }
