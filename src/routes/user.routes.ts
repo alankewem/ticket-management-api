54e8921ab9059b0dd84e1ab4be2abc9d203ccd3d
kewem <kewemcg@gmail.com>
2022-06-30 15:08:28 -0400

    feat: POST route for user login

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { checkSchema, validationResult } from "express-validator";
+import { authJWT } from "../middleware/auth";
 import { createUserController } from "../useCases/CreateUser";
+import { loginUserController } from "../useCases/LoginUser";
 
 const router = Router();
 
 router.post(
   "/",
+  authJWT,
   checkSchema({
     name: { isString: true, notEmpty: true },
     email: { isEmail: true, normalizeEmail: true, notEmpty: true },
@@ -22,5 +25,19 @@ router.post(
     return await createUserController.handle(request, response, next);
   }
 );
+
+router.post("/login", checkSchema({
+  email: { isString: true },
+  password: { isString: true }
+}),
+  async (request: Request, response: Response, next: NextFunction) => {
+    const errors = validationResult(request);
+
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+
+    return await loginUserController.handle(request, response, next);
+  })
 
 export default router;
