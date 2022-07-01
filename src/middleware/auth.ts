e9ed65f4e992130fdad5c1adf939fe8f78873a83
kewem <kewemcg@gmail.com>
2022-07-01 04:59:36 -0400

    feat: jwt validation and role authorization check

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,28 +1,33 @@
 import { NextFunction, Request, Response } from "express";
-import { verify, JwtPayload } from "jsonwebtoken";
+import { JwtPayload, verify } from "jsonwebtoken";
+import { IRoles } from "../entities/User";
 
-export function authJWT(req: Request, res: Response, next: NextFunction) {
-    if (req.method === "OPTIONS") {
+export function authJWT(roles: IRoles[]) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (req.method === "OPTIONS") {
+            return next();
+        }
+
+        if (!req.headers.authorization) {
+            throw res.status(400).json({ error: "Authentication failed!" });
+        }
+
+        const headerAuth = req.headers.authorization!;
+
+        const token = headerAuth.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+
+        if (!token) {
+            throw res.status(400).json({ error: "Authentication failed!" });
+        }
+
+        const decoded = verify(token, `${process.env.JWT_SECRET}`) as JwtPayload;
+
+        const authorized = roles.includes(decoded.user)
+
+        if (!authorized) {
+            return res.status(401).json({ error: "Unauthorized" })
+        }
+
         return next();
     }
-
-    if (!req.headers.authorization) {
-        throw res.status(400).json({ error: "Authentication failed!" });
-    }
-
-    const headerAuth = req.headers.authorization!;
-
-    const token = headerAuth.split(" ")[1]; // Authorization: 'Bearer TOKEN'
-
-    if (!token) {
-        throw res.status(400).json({ error: "Authentication failed!" });
-    }
-
-    const decoded = verify(token, `${process.env.JWT_SECRET}`) as JwtPayload;
-
-    console.log(decoded)
-
-    Object.assign(req.query.user, decoded);
-
-    return next();
 }
