4e073c1986759b65a51c537240a0ee3d875e6f63
kewem <kewemcg@gmail.com>
2022-07-01 05:09:50 -0400

    feat: new method to create administrative users

--- src/useCases/CreateUser/CreateUserController.ts
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -2,14 +2,27 @@ import { NextFunction, Request, Response } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 export class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+  constructor(private createUserUseCase: CreateUserUseCase) { }
 
-  async handle(req: Request, res: Response, next: NextFunction): Promise<Response> {
+  async common(req: Request, res: Response, next: NextFunction): Promise<Response> {
+    const { name, email, password, phone } = req.body;
+
+    try {
+      const user = await this.createUserUseCase.execute({ name, email, password, phone, role: "common" }).catch(error => {
+        res.status(400).json({ error: error })
+      })
+      return res.status(201).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error || "Unexpected error" });
+    }
+  }
+
+  async admin(req: Request, res: Response, next: NextFunction): Promise<Response> {
     const { name, email, password, phone, role } = req.body;
 
     try {
-      const user = await this.createUserUseCase.execute({ name, email, password, phone, role }).catch(error=> {
-        res.status(400).json({error: error})
+      const user = await this.createUserUseCase.execute({ name, email, password, phone, role }).catch(error => {
+        res.status(400).json({ error: error })
       })
       return res.status(201).json(user);
     } catch (error) {
