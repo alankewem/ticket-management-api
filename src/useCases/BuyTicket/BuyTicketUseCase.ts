47d609de2bd5712f8f1204395ef027b7bb67feca
kewem <kewemcg@gmail.com>
2022-07-01 14:50:09 -0400

    feat: check the amount of tickets available

--- src/useCases/BuyTicket/BuyTicketUseCase.ts
+++ src/useCases/BuyTicket/BuyTicketUseCase.ts
@@ -1,12 +1,20 @@
 import { Ticket } from "../../entities/Ticket";
+import { IEventRepository } from "../../repository/IEventRepository";
 import { ITicketRepository } from "../../repository/ITicketRepository";
 import { IBuyTicketDTO } from "./BuyTicketDTO";
 
 export class BuyTicketUseCase {
-    constructor(private ticketRepository: ITicketRepository) { }
+    constructor(private ticketRepository: ITicketRepository, private eventRepository: IEventRepository) { }
 
     async execute(data: IBuyTicketDTO) {
         try {
+            const event = await this.eventRepository.findById(data.event)
+            const tickets = await this.ticketRepository.findByEvent(data.event)
+
+            if (event.ticketsAvailable <= tickets.length) { 
+                return Promise.reject("Tickets Sold Out")
+            }
+
             const ticket = new Ticket(data)
 
             const createdTicket = await this.ticketRepository.save(ticket);
--- src/useCases/BuyTicket/index.ts
+++ src/useCases/BuyTicket/index.ts
@@ -1,8 +1,9 @@
 import { ticketRepository } from "../../repository/implementations/TicketRepository";
+import { eventRepository } from "../../repository/implementations/EventRepository";
 import { BuyTicketUseCase } from "./BuyTicketUseCase";
 import { BuyTicketController } from "./BuyTicketController";
 
-const buyTicketUseCase = new BuyTicketUseCase(ticketRepository)
+const buyTicketUseCase = new BuyTicketUseCase(ticketRepository, eventRepository)
 const buyTicketController = new BuyTicketController(buyTicketUseCase)
 
 export { buyTicketController, buyTicketUseCase }
